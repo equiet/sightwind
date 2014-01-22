7d6a3cd1035b9f3e3a84dcb42fa9c828a29937a6
Jakub Jurových <jakub.jurovych@gmail.com>
2014-01-22 13:35:10 +0100

    Move WebGL heatmap to separate file and disable

--- public/js/main.js
+++ public/js/main.js
@@ -485,8 +485,6 @@ d3.csv('data/frames.csv', function(err, frames) {
 
     loadData(currentFrame, -1).then(function() {
 
-        // runWebGL();
-
         // var frame = 0;
         // interval = setInterval(function() {
         //     loadData(++frame % 72);
@@ -749,82 +747,3 @@ Q(function() {
 
 
 }).done();
-
-
-
-
-function runWebGL() {
-
-    var $container = document.querySelector('.container');
-
-    // set the scene size
-    var WIDTH = $container.clientWidth,
-        HEIGHT = $container.clientHeight;
-
-
-    // create a WebGL renderer, camera
-    // and a scene
-    var renderer = new THREE.WebGLRenderer();
-    var camera = new THREE.OrthographicCamera( WIDTH / -2, WIDTH / 2, HEIGHT / 2, HEIGHT / -2, 0, 1000);
-    var scene = new THREE.Scene();
-
-    // the camera starts at 0,0,0 so pull it back
-    camera.position.z = 100;
-
-    // start the renderer
-    renderer.setSize(WIDTH, HEIGHT);
-
-    // attach the render-supplied DOM element
-    $container.appendChild(renderer.domElement);
-    renderer.domElement.classList.add('is-heatmap');
-
-    var attributes = {
-        temperature: { type: 'f', value: [] },
-    };
-
-    var uniforms = {
-        startColor: { type: 'c', value: new THREE.Color(0x124156) },
-        endColor: { type: 'c', value: new THREE.Color(0x1e0829) },
-        // endColor: { type: 'c', value: new THREE.Color(0x1e0829) },
-        minTemperature: { type: 'f', value: options.colorScale[0].color },
-        maxTemperature: { type: 'f', value: options.colorScale[options.colorScale.length - 1].color }
-    };
-
-    // create the sphere's material
-    var shaderMaterial = new THREE.ShaderMaterial({
-        uniforms:       uniforms,
-        attributes:     attributes,
-        vertexShader:   document.querySelector('#vertexshader').innerHTML,
-        fragmentShader: document.querySelector('#fragmentshader').innerHTML
-    });
-
-    // set up the sphere vars
-    var radius = 50, segments = 16, rings = 16;
-
-
-    var geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, DATA_WIDTH - 1, DATA_HEIGHT - 1);
-    var plane = new THREE.Mesh(geometry, shaderMaterial);
-    scene.add(plane);
-
-
-    // // now populate the array of attributes
-    for (var v = 0; v < plane.geometry.vertices.length; v++) {
-        attributes.temperature.value[v] = data.temp2m[v];
-    }
-
-
-
-    var frame = 0;
-
-
-
-    function render() {
-
-        renderer.render(scene, camera);
-        // requestAnimationFrame(render);
-
-    }
-    render();
-
-
-}
