e1b98fba39a1bc49d298103d27dc50ab19ae293c
Jakub Jurových <jakub.jurovych@gmail.com>
2014-11-30 03:52:27 +0100

    Cleanup Capistrano and Dokku deploy configs

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-set :stage, :production
-
-set :deploy_to, '/var/www/sightwind.com'
-
-role :app, %w{ubuntu@dev1.ometfn.net}
-role :web, %w{ubuntu@dev1.ometfn.net}
-
-server 'dev1.ometfn.net', user: 'ubuntu', roles: %w{web app}, my_property: :my_value, port: 22001
-
