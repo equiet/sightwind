9467ed4a0426ecb5cc357cb796a25b1d78f63355
Jakub Jurových <jakub.jurovych@gmail.com>
2015-01-13 05:51:43 +0100

    Simplify update script, remove dependency on loading last call from undocumented API

--- update.sh
+++ update.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-RUN=$(curl http://api.omd.li/runs/last)
-
-TIME_NOW=$(python <<END
-import time
-from datetime import datetime
-print str(int(time.mktime(datetime.now().timetuple())))
-END)
 TIME_RUN=$(python <<END
-import time
 from datetime import datetime
-last_run = datetime.strptime("${RUN}", "%Y%m%d%H")
-print str(int(time.mktime(last_run.timetuple())))
+today = datetime.today()
+print str(int((datetime(today.year, today.month, today.day, 0, 0) - datetime(1970,1,1)).total_seconds()))
 END)
-
-DELTA=$(( TIME_NOW - TIME_RUN ))
-FRAME=$(( DELTA / 3600 ))
-REMAIN=$(( DELTA % 3600 ))
-
-if [ "$REMAIN" -ge "1800" ]; then
-  FRAME=$(( FRAME+1 ))
-fi
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
@@ -37,7 +21,7 @@ do
 	# VARS="wind10m_u,wind10m_v,topo,rain,temp2m,press[0],lat,lon"
 	# VARS="wind10m_u,wind10m_v,temp2m,wind_u,wind_v,temp"
 	VARS="wind10m_u,wind10m_v,temp2m"
-	wget "http://dap.ometfn.net/eu12-pp_${RUN}_${FRAME}.nc.nc?$VARS" -O data_tmp.nc \
+	wget "http://dap.ometfn.net/eu12-pp_$(date +%Y%m%d00)_${FRAME}.nc.nc?$VARS" -O data_tmp.nc \
 		&& python nc-to-img.py $FRAME \
 		&& echo "${TIME_FRAME},${FRAME}" >> public/data/frames.csv \
 
